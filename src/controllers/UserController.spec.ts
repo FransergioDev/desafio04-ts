ff21beebda87e4f8a9e6c6940ef636fbcdfa3021
FransergioDev <fransergio.dev@gmail.com>
2023-08-03 10:58:50 -0300

    Part 1  of challenge completed

--- src/controllers/UserController.spec.ts
+++ src/controllers/UserController.spec.ts
@@ -4,8 +4,24 @@ import { Request } from 'express'
 import { makeMockResponse } from "../__mocks__/mockResponse.mock";
 
 describe('UserController', () => {
+    const mockListUsers = [
+        {
+            name: "Joana",
+            email: "joana@dio.com",
+        },
+        {
+            name: "Pedro",
+            email: "pedro@dio.com",
+        },
+        {
+            name: "Ana",
+            email: "ana@dio.com",
+        },
+    ];
+
     const mockUserService: Partial<UserService> = {
-        createUser: jest.fn()
+        createUser: jest.fn(),
+        getAllUsers: jest.fn().mockReturnValue(mockListUsers)
     }
     
     const userController = new UserController(mockUserService as UserService);
@@ -21,5 +37,30 @@ describe('UserController', () => {
         userController.createUser(mockRequest, mockResponse)
         expect(mockResponse.state.status).toBe(201)
         expect(mockResponse.state.json).toMatchObject({ message: 'Usuário criado' })
+    })
+
+    it('Deve retornar erro na tentativa de adicionar um novo usuário com campo nome inválido', () => {
+        const mockRequest = {
+            body: {
+                name: null,
+                email: 'nath@test.com'
+            }
+        } as Request
+        const mockResponse = makeMockResponse()
+        userController.createUser(mockRequest, mockResponse)
+        expect(mockResponse.state.status).toBe(400)
+        expect(mockResponse.state.json).toMatchObject({ message: 'Bad request! Name obrigatório' })
+    })
+
+
+    it('Deve retornar uma lista de usuários', () => {
+        const mockRequest = {} as Request
+        const mockResponse = makeMockResponse();
+        const mockUserServiceSpyOn = jest.spyOn(mockUserService, 'getAllUsers');
+        userController.getAllUsers(mockRequest, mockResponse)
+        expect(mockResponse.state.status).toBe(200);
+        expect(mockResponse.state.json).not.toBeNull();
+        expect(mockUserServiceSpyOn).toHaveBeenCalled();
+        expect(mockResponse.state.json).toMatchObject(mockListUsers);
     })
 })
